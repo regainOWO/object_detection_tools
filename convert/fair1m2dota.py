a4e396b46fc0d4cff50b05591c12b7f16923f6ee
RegainOAO <kk290291904@163.com>
2023-11-10 14:16:06 +0800

    update fair1m2dota

--- convert/fair1m2dota.py
+++ convert/fair1m2dota.py
@@ -1,64 +1,63 @@
 """
 此脚本的主要内容
-* 将fair1m的数据格式转换成dota格式
-* points的数据默认是像素坐标，还未支持地理坐标转像素坐标
-
-数据集目录
-.
-├── images
-│   ├──xxx.tif
-│   └──......
-└── labelXml
-    ├──xxx.xml
-    └──......
+1. 将fair1m的数据格式转换成dota格式
+2. 标签名称中间如果有空格，则名称中间的空格字符则将替换成'-'符号；如 tennis racket -> tennis-racket
 
 使用方法，需要输入三个参数：
---dataset-dir: 数据集的根路径
---image-dirname: 数据集根路径下存放所有图片的文件夹名称
---anno-dirname: 数据集根路径下存放所有xml标签的文件夹名称
+--ann-dir: pascal voc的xml标签文件存放的路径
+--output-dir: dota格式输出的路径
+--noempty: 是否保存空标签文件，默认是保存的
 """
+import argparse
+import glob
 import logging
 import xml.etree.ElementTree as ET
 import os
-from pathlib import Path
+import os.path as osp
 
 from tqdm import tqdm
-import argparse
 
-IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 'pfm'  # include image suffixes
-CLASS_NAMES = {}
+
+def get_logger(log_name):
+    logger = logging.getLogger(log_name)
+    if logger.handlers:
+        return logger
+    ch = logging.StreamHandler()
+    ch_format = logging.Formatter('[%(levelname)s %(asctime)s] %(message)s')
+    ch.setFormatter(ch_format)
+    logger.addHandler(ch)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+# logger
+LOGGER = get_logger('fair1m2dota')
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset-dir', type=str, required=True,
-                        help='input dataset dir')
-    parser.add_argument('--image-dirname', type=str, default='images',
-                        help='image directory name under the dataset dir')
-    parser.add_argument('--label-dirname', type=str, default='labelXml',
-                        help='annotation directory name under the dataset dir')
+    parser.add_argument('--ann-dir', type=str, required=True,
+                        help='input pascal voc annotations dir')
     parser.add_argument('--output-dir', type=str, default=None,
-                        help='output directory path, default is labelTxt directory under the dataset dir')
+                        help='output yolo annotation dir, default is beside the ann-dir directory called labels')
+    parser.add_argument('--noempty', action='store_true',
+                        help='if or not save empty label, default is not save')
     opt = parser.parse_args()
     return opt
 
 
-def xml2dota_txt(image_dir, xml_file, out_file):
+def xml2dota_txt(xml_file, out_file, class_names, save_empty):
     """
+    将fair1m的.xml标签文件转成 dota标签的.txt标签文件
     Args:
-        image_dir (str): 图片文件夹的路径
-        xml_file (str): xml标签文件的路径
-        out_file (str): 输出标签文件的路径
+        xml_file (str): pascal voc标签文件的路径
+        out_file (str): dota标签文件的输出路径
+        class_names (list): 标签类别名称
+        save_empty (bool): 是否保存空标签
     """
-    global CLASS_NAMES
     in_file = open(xml_file)
     tree = ET.parse(in_file)
     root = tree.getroot()
-    source = root.find('source')
-    image_filename = Path(source.find('filename').text)  # maybe unvaluable, just use the suffix
-    image_suffix = os.path.splitext(image_filename)[-1].split('.')[-1]
-    assert image_suffix in IMG_FORMATS, f'{image_filename} is not image type, please check your dataset'
-    image_file = os.path.join(image_dir, image_filename)
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
@@ -69,62 +68,69 @@ def xml2dota_txt(image_dir, xml_file, out_file):
         is_pixel_point = True
         if obj.find('coordinate').text != 'pixel':
             is_pixel_point = False
-        class_name = obj.find('possibleresult').find('name').text
-        class_name = class_name.strip().replace(' ', '-')   # 将类别名称中间的空格字符替换成 -
+        name = obj.find('possibleresult').find('name').text
+        name = name.strip().replace(' ', '-')   # 将类别名称中间的空格字符替换成 -
         # 用于打印数据集中的总共的类别名称
-        if class_name not in CLASS_NAMES.keys():
-            CLASS_NAMES[class_name] = 1
-        else:
-            CLASS_NAMES[class_name] += 1
+        if name not in class_names:
+            class_names.append(name)
         points = obj.find('points')
         poly = []
         for point in points.findall('point')[:4]:
             x, y = map(float, point.text.split(','))
-            if not is_pixel_point:
-                logging.warning('')
             poly.append(x)
             poly.append(y)
-        one_line = " ".join([str(a) for a in poly]) + " " + class_name + " " + str(difficult) + "\n"
+        # 非像素坐标值，目前无法转化
+        if not is_pixel_point:
+            LOGGER.warning(f"file: {xml_file} coordinate is not in pixel")
+            LOGGER.warning("not support geometry coordinate transfer to pixel coodinate yet! this label will be ignore")
+            continue
+        one_line = " ".join([str(a) for a in poly]) + " " + name + " " + str(difficult) + "\n"
         content_lines.append(one_line)
     if len(content_lines) == 0:
-        logging.warning(f'no lines to save, the file: {out_file} is empty')
-    # 将内容写入文件
-    with open(out_file, 'w', encoding='utf-8') as f:
-        f.writelines(content_lines)
+        LOGGER.warning(f'no lines to save, the file: {out_file} is empty')
+    if len(content_lines) > 0 or save_empty:
+        # 将内容写入文件
+        with open(out_file, 'w', encoding='utf-8') as f:
+            f.writelines(content_lines)
 
 
-def run(dataset_dir, image_dirname, label_dirname, output_dir):
-    # 读取类别信息
-    image_dir = os.path.join(dataset_dir, image_dirname)
-    label_dir = os.path.join(dataset_dir, label_dirname)
-    label_files = []
-    for label_filename in os.listdir(label_dir):
-        if os.path.splitext(label_filename)[-1] != '.xml':
-            continue
-        label_file = os.path.join(label_dir, label_filename)
-        label_files.append(label_file)
-    assert len(label_files), f"{label_dir}下没有xml标签文件"
-    if output_dir is None or not isinstance(output_dir, str):
-        output_dir = os.path.join(dataset_dir, 'labelTxt')
+def save_classes(classes_file, class_names):
+    """保存classes.txt文件"""
+    with open(classes_file, 'w', encoding='utf-8') as f:
+        f.writelines("\n".join(class_names) + '\n')
+
+
+def run(ann_dir, output_dir, save_empty=True):
+    # 数据集所包含的标签类别名称
+    class_names = []
+    if output_dir is None:
+        output_dir = osp.join(osp.dirname(ann_dir), 'labelTxt')
     # 创建生成label的文件夹
     os.makedirs(output_dir, exist_ok=True)
-    for label_file in tqdm(label_files, total=len(label_files), desc=f'convert fair1m label to dota label'):
-        filename = Path(label_file).stem
-        dst_file = os.path.join(output_dir, filename + '.txt')  # 生成的文件
-        xml2dota_txt(image_dir, label_file, dst_file)
-    logging.info("convert fair1m label to dota label success!")
-    # 生成classes.txt文件
-    class_names = list(CLASS_NAMES.keys())
+    # 后去所有的标签源文件
+    ann_files = glob.glob(ann_dir + '/*.xml')
+    assert len(ann_files) > 0, f"path: {ann_dir} dose not have any .xml file"
+    # 标签文件转化
+    pbar = tqdm(ann_files, total=len(ann_files))
+    for ann_file in pbar:
+        pbar.set_description(f'process {ann_file}')
+        output_filename = osp.splitext(osp.basename(ann_file))[0] + '.txt'
+        output_file = osp.join(output_dir, output_filename)
+        xml2dota_txt(ann_file, output_file, class_names, save_empty)
+    # save classes.txt
+    classes_file = osp.join(osp.dirname(output_dir), 'classes.txt')
     class_names.sort()
-    with open(os.path.join(dataset_dir, 'classes.txt'), mode='w', encoding='utf-8') as f:
-        content_lines = [x + '\n' for x in class_names]
-        f.writelines(content_lines)
-    logging.info(f"Success generate classes.txt in {os.path.abspath(dataset_dir)}")
+    save_classes(classes_file, class_names)
+    # print msg
+    LOGGER.info(f"dataset total class_names: {class_names}")
+    LOGGER.info(f"classes.txt saved in: {classes_file}")
+    LOGGER.info(f"dota type label saved in: {output_dir}")
 
 
 if __name__ == "__main__":
     opt = parse_opt()
-    run(dataset_dir=opt.dataset_dir,
-        image_dirname=opt.image_dirname,
-        label_dirname=opt.label_dirname,
-        output_dir=opt.output_dir)
+    run(ann_dir=opt.ann_dir,
+        output_dir=opt.output_dir,
+        save_empty=not opt.noempty)
+
+# E:/GithubProjects/jp-sample/mini_fair1m/train/labelXml
